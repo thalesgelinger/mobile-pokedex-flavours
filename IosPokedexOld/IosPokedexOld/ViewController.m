d0c8a7940c5e14dfb52cc77deaf9cdd4d7ada507
Thales Gelinger <thalesgelinger@gmail.com>
2023-10-03 19:21:59 -0300

    feat: refact header

--- IosPokedexOld/IosPokedexOld/ViewController.m
+++ IosPokedexOld/IosPokedexOld/ViewController.m
@@ -1,5 +1,6 @@
 #import "ViewController.h"
 #include "Colors.h"
+#include "Header.h"
 #include "Icon.h"
 #include "TextInput.h"
 
@@ -16,53 +17,9 @@
 
   self.view.backgroundColor = [Colors primary];
 
-  UIView *header = [[UIView alloc]
+  Header *header = [[Header alloc]
       initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 100)];
-  header.translatesAutoresizingMaskIntoConstraints = NO;
   [self.view addSubview:header];
-
-  Icon *pokeball =
-      [[Icon alloc] initWithName:@"pokeball"
-                           color:[[Colors grayscale] objectForKey:@"white"]
-                            size:20];
-  pokeball.translatesAutoresizingMaskIntoConstraints = NO;
-  [header addSubview:pokeball];
-
-  CGFloat pixelValue = 16;
-  CGFloat scaleFactor = [UIScreen mainScreen].scale;
-  CGFloat pointValue = pixelValue * scaleFactor;
-
-  UILabel *pokedexTitle = [[UILabel alloc] init];
-  pokedexTitle.translatesAutoresizingMaskIntoConstraints = NO;
-  [header addSubview:pokedexTitle];
-
-  pokedexTitle.text = @"Pokedex";
-  pokedexTitle.textColor = [[Colors grayscale] objectForKey:@"white"];
-  pokedexTitle.textAlignment = NSTextAlignmentCenter;
-
-  [pokedexTitle sizeToFit];
-
-  pokedexTitle.font = [UIFont boldSystemFontOfSize:12.0];
-  pokedexTitle.center = CGPointMake(20, pointValue);
-
-  TextInput *searchInput =
-      [[TextInput alloc] initWithFrame:CGRectMake(50, 100, 200, 30)];
-  searchInput.translatesAutoresizingMaskIntoConstraints = NO;
-  searchInput.placeholder = @"Search";
-  searchInput.delegate = self;
-  searchInput.backgroundColor = [[Colors grayscale] objectForKey:@"white"];
-  searchInput.layer.cornerRadius = 16;
-  searchInput.layer.borderWidth = 0.0;
-  searchInput.layer.masksToBounds = YES;
-
-  [self.view addSubview:searchInput];
-
-  Icon *search = [[Icon alloc] initWithName:@"search"
-                                      color:[Colors primary]
-                                       size:20];
-
-  search.translatesAutoresizingMaskIntoConstraints = NO;
-  [searchInput addSubview:search];
 
   [NSLayoutConstraint activateConstraints:@[
     [header.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor
@@ -70,32 +27,6 @@
     [header.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor
                                           constant:-20],
     [header.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:50],
-    [header.heightAnchor constraintEqualToConstant:40],
-
-    [pokeball.leadingAnchor constraintEqualToAnchor:header.leadingAnchor
-                                           constant:0],
-    [pokeball.topAnchor constraintEqualToAnchor:header.topAnchor constant:0],
-    [pokeball.heightAnchor constraintEqualToAnchor:header.heightAnchor
-                                          constant:0],
-    [pokeball.widthAnchor constraintEqualToAnchor:header.heightAnchor
-                                         constant:0],
-
-    [pokedexTitle.leadingAnchor constraintEqualToAnchor:pokeball.trailingAnchor
-                                               constant:20],
-    [pokedexTitle.centerYAnchor constraintEqualToAnchor:header.centerYAnchor],
-
-    [searchInput.leadingAnchor constraintEqualToAnchor:header.leadingAnchor],
-    [searchInput.trailingAnchor constraintEqualToAnchor:header.trailingAnchor],
-    [searchInput.topAnchor constraintEqualToAnchor:header.bottomAnchor
-                                          constant:8],
-    [searchInput.heightAnchor constraintEqualToConstant:32],
-
-    [search.heightAnchor constraintEqualToConstant:16],
-    [search.widthAnchor constraintEqualToConstant:16],
-    [search.leadingAnchor constraintEqualToAnchor:searchInput.leadingAnchor
-                                         constant:12],
-    [search.centerYAnchor constraintEqualToAnchor:searchInput.centerYAnchor],
-
   ]];
 
   UICollectionViewFlowLayout *flowLayout =
@@ -118,7 +49,7 @@
   [self.view addSubview:collectionView];
 
   [NSLayoutConstraint activateConstraints:@[
-    [collectionView.topAnchor constraintEqualToAnchor:searchInput.bottomAnchor
+    [collectionView.topAnchor constraintEqualToAnchor:header.bottomAnchor
                                              constant:24],
     [collectionView.leadingAnchor
         constraintEqualToAnchor:self.view.leadingAnchor
--- IosPokedexOld/Pods/Target Support Files/Pods-IosPokedexOld/Pods-IosPokedexOld-frameworks.sh
+++ IosPokedexOld/Pods/Target Support Files/Pods-IosPokedexOld/Pods-IosPokedexOld-frameworks.sh
@@ -176,12 +176,12 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SVGKit/SVGKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Yoga/yoga.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/YogaKit/YogaKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SVGKit/SVGKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Yoga/yoga.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/YogaKit/YogaKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
